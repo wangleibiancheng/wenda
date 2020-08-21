4f2b15ec756ed8d0fe20919341e66f16b4331d16
wanglei <2335837493@qq.com>
2020-08-21 18:35:48 +0800

    1

---
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -0,0 +1,24 @@
+package life.majiang.community.community.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+/**
+ * Created by codedrinker on 2019/4/24.
+ */
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "10") Integer size,
+                        @RequestParam(name = "search", required = false) String search,
+                        @RequestParam(name = "tag", required = false) String tag,
+                        @RequestParam(name = "sort", required = false) String sort) {
+
+        return "index";
+    }
+}
