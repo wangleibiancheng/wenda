306f6359e000bc110cc564f484c02e9c1bb062a6
wanglei <2335837493@qq.com>
2020-09-01 12:28:06 +0800

    拦截器后边需要看

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -10,9 +10,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
+
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
+
 
 /**
  * Created by codedrinker on 2019/4/24.
@@ -28,19 +28,6 @@ public class IndexController {
                         @RequestParam(name = "page",defaultValue = "1")Integer page,
                         @RequestParam(name = "size", defaultValue = "5") Integer size,
                         Model model) {
-        Cookie[] cookies = request.getCookies();
-        if(cookies!=null&&cookies.length>0){//增加判断
-            for (Cookie cookie:cookies){
-                if(cookie.getName().equals("token")){
-                    String token=cookie.getValue();
-                    User user =userMapper.findByToken(token);
-                    if(user!=null){
-                        request.getSession().setAttribute("user",user);
-                    }
-                    break;
-                }
-            }
-        }
         PaginationDTO pagination = questionService.list(page,size);
         model.addAttribute("pagination", pagination);
         return "index";
---
+++ src/main/java/life/majiang/community/community/interceptor/WebConfig.java
@@ -0,0 +1,26 @@
+package life.majiang.community.community.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author wanglei
+ * @date 2020/9/1 12:16 下午
+ * @File : WebConfig.java
+ */
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private SessionInterceptor sessionInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(sessionInterceptor).addPathPatterns("/**");
+    }
+}
