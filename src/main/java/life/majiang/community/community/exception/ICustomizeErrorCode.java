da507df35724542e65bec0c24e8d2550e892fc28
wanglei <2335837493@qq.com>
2020-09-02 20:20:57 +0800

    异常处理

--- src/main/java/life/majiang/community/community/exception/ICustomizeErrorCode.java
+++ src/main/java/life/majiang/community/community/exception/ICustomizeErrorCode.java
@@ -2,10 +2,10 @@ package life.majiang.community.community.exception;
 
 /**
  * @author wanglei
- * @date 2020/9/2 3:59 下午
+ * @date 2020/9/2 8:18 下午
  * @File : ICustomizeErrorCode.java
  */
-//传第的是父类的引用，所以定成接口
+
 public interface ICustomizeErrorCode {
     String getMessage() ;
 }
